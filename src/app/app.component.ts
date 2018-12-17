3abf1e20927583db3278d58972774c35103f0d3b
Marcos Lopez <rippleytrigger@gmail.com>
2018-12-17 17:38:54 -0400

    Ajustes Tarea

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
+})
+export class AppComponent {
+  inactiveUsers
+  activeUsers
+
+  constructor(private userService: UserService) {
+    this.inactiveUsers = this.userService.inactiveUsers
+    this.activeUsers = this.userService.activeUsers
+  }
+
+  onSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
+  onSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
+}
